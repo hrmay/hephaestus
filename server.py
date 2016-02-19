d139732aac3f4283fd184a2497328058ca9d0902
mmclark317 <mmclark317@gmail.com>
2016-02-19 19:56:19 +0000

    Added Start Bootstrap template and elements, Flaskified

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def helloWorld():
-    return "Hello World!"
+def mainIndex():
+    return render_template("index.html");
     
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug = True)
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug = True)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               

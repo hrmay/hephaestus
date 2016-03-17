8f8777411568675fbe7a3f7649e094f576d04eaf
mmclark317 <mmclark317@gmail.com>
2016-03-17 03:26:08 +0000

    Working on article and user template HTML/CSS

--- server.py
+++ server.py
@@ -9,5 +9,9 @@ def mainIndex():
     header_img = random.randint(1,5);
     return render_template("index.html", header_num = header_img);
     
+@app.route('/article_test')
+def article():
+    return render_template("article.html");
+
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug = True)

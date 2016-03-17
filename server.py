71d14ee5b7698b1b896ed2d4fb2dca16f3614881
mmclark317 <mmclark317@gmail.com>
2016-03-17 14:40:54 +0000

    Added full user template

--- server.py
+++ server.py
@@ -13,7 +13,6 @@ def connectToDB():
     except:
         print("Can't connect to database.")
 
-
 @app.route('/')
 def mainIndex():
     header_img = random.randint(1,5);
@@ -22,6 +21,10 @@ def mainIndex():
 @app.route('/article_test')
 def article():
     return render_template("article.html");
+    
+@app.route('/user_test')
+def user():
+    return render_template("user.html");
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug = True)

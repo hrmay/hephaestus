587721d5478d8ee3ac182fa69fc821dae54ee251
mmclark317 <mmclark317@gmail.com>
2016-03-24 20:00:22 -0400

    Accordions are working!!

--- static/js/accordion.js
+++ static/js/accordion.js
@@ -1,18 +1,12 @@
-var accordion = function() {
-    $('.accordion-content').slideUp('fast');
-    $('#world-nav').find('.accordion-toggle').on('click', function() {
-        window.console&&console.log('Clicked');
-        if ($(this).next().is(':visible')) {
-            window.console&&console.log('Sliding up other content');
-            $(this).next().slideUp('fast');
-        } else {
-            window.console&&console.log('Sliding down other content');
-            $(this).next().slideDown('fast');
-        }
-        $('.accordion-content').not($(this).next()).slideUp('fast');
+$(document).ready(function($) {
+    $('#world-nav').find('.accordion-toggle').click(function() {
+        
+        //Expand or collapse this panel
+        $(this).next().slideToggle('fast');
+        
+        //Hide the other panels
+        $(".accordion-content").not($(this).next()).slideUp('fast');
+        
+        console.log("Here");
     });
-}
-
-$(document).ready(function() {
-    accordion();
 });
